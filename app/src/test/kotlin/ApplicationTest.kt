f004655dd42d1db795a96218498135b4468d8213
L5249 <lewisa2wiles@gmail.com>
2026-03-07 10:23:10 +0000

    Initial base template created ready for development

---
+++ app/src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.Assertions.assertTrue
+
+class ApplicationTest {
+    @Test
+    fun sanityTest() {
+        assertTrue(true)
+    }
+}
