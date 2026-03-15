29302a00bd7e8affa20644e39cb26f71b47465d9
Folakemi-Fareedah-Fashola <bqbq7257@leeds.ac.uk>
2026-03-15 18:02:32 +0000

    Necessary imports and routing added

--- app/src/main/kotlin/Application.kt
+++ app/src/main/kotlin/Application.kt
@@ -2,6 +2,8 @@ package diettracker
 
 import io.ktor.server.application.*
 import io.ktor.server.netty.EngineMain
+import io.ktor.server.pebble.*
+import io.pebbletemplates.pebble.loader.ClasspathLoader
 import diettracker.db.DatabaseFactory
 
 fun main(args: Array<String>) {
@@ -9,6 +11,11 @@ fun main(args: Array<String>) {
 }
 
 fun Application.module() {
+    install(Pebble) {
+        loader(ClasspathLoader().apply {
+            prefix = "templates"  // Add this line
+        })
+    }
     DatabaseFactory.init()
 
     configureAuthentication()
