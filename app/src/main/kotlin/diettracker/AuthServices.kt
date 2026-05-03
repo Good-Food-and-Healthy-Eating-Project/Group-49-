0c4f3f3b35e2a428f9721c73a247fa89b6641261
Folakemi-Fareedah-Fashola <bqbq7257@leeds.ac.uk>
2026-05-03 03:08:13 +0100

    Further constrast issues fixed

--- app/src/main/kotlin/diettracker/AuthServices.kt
+++ app/src/main/kotlin/diettracker/AuthServices.kt
@@ -159,7 +159,7 @@ suspend fun ApplicationCall.logout() {
     val email = sessions.get<UserSession>()?.email.toString()
     application.log.info("User $email logged out")
     sessions.clear<UserSession>()
-    respondRedirect("/landing_page")
+    respondRedirect("/")
 }
 
 suspend fun ApplicationCall.getCredentials(): Pair<String, String> {
