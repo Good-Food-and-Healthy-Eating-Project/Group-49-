28bf0d059323ac94f89f85854c111ac976e26df9
Folakemi-Fareedah-Fashola <bqbq7257@leeds.ac.uk>
2026-05-08 13:58:01 +0100

    Fixsed test failures

--- app/src/main/kotlin/diettracker/routing/ClientDashRoutes.kt
+++ app/src/main/kotlin/diettracker/routing/ClientDashRoutes.kt
@@ -1,7 +1,7 @@
 package diettracker.routing
 
-import diettracker.UserSession
-import diettracker.getUserIdByEmail
+import diettracker.db.repositories.getUserIdByEmail
+import diettracker.services.UserSession
 import diettracker.services.buildClientDashModel
 import io.ktor.server.pebble.PebbleContent
 import io.ktor.server.response.respond
@@ -15,9 +15,6 @@ import io.ktor.server.sessions.sessions
  * This page contains all the routing functions used for the client dashboard page
  * The Client dashboard displays guidance messages based on UK recommended nutritional guidelines
  * The messages help the user know if they are following recommended guidelines
- * AC-ATH-01, AC-ATH-07, AC-ATH-08, AC-ATH-11, AC-ATH-01, AC-ATH-07, AC-ATH-08, AC-ATH-11
- * AC-STUDENT-03, AC-STUDENT-04, AC-STUDENT-06, AC-STUDENT-07, AC-STUDENT-08, AC-STUDENT-10
- * AC-VEG-05
 */
 fun Route.configureClientDashRoute() {
     get("/client_dash") {
