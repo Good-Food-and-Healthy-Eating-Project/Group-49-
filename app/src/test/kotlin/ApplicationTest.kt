ee0136b21046843cb406ab7fafaabb034175fb34
L5249 <lewisa2wiles@gmail.com>
2026-05-08 14:27:25 +0100

    cleaned main

--- app/src/test/kotlin/ApplicationTest.kt
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.Assertions.assertTrue
-
-class ApplicationTest {
-    @Test
-    fun sanityTest() {
-        assertTrue(true)
-    }
-}
