bc4604772eeab4687fb2dea414c259e855dcf2a6
Folakemi-Fareedah-Fashola <bqbq7257@leeds.ac.uk>
2026-05-08 04:28:52 +0100

    Aria labels added + new contrast issues which were found are fixed

--- app/src/main/kotlin/diettracker/routing/ClientDashRoutes.kt
+++ app/src/main/kotlin/diettracker/routing/ClientDashRoutes.kt
@@ -15,18 +15,10 @@ import io.ktor.server.sessions.sessions
  * This page contains all the routing functions used for the client dashboard page
  * The Client dashboard displays guidance messages based on UK recommended nutritional guidelines
  * The messages help the user know if they are following recommended guidelines
- **/
-// AC-ATH-01
-// AC-ATH-07
-// AC-ATH-08
-// AC-ATH-11
-// AC-STUDENT-03
-// AC-STUDENT-04
-// AC-STUDENT-06
-// AC-STUDENT-07
-// AC-STUDENT-08
-// AC-STUDENT-10
-// AC-VEG-05
+ * AC-ATH-01, AC-ATH-07, AC-ATH-08, AC-ATH-11, AC-ATH-01, AC-ATH-07, AC-ATH-08, AC-ATH-11
+ * AC-STUDENT-03, AC-STUDENT-04, AC-STUDENT-06, AC-STUDENT-07, AC-STUDENT-08, AC-STUDENT-10
+ * AC-VEG-05
+*/
 fun Route.configureClientDashRoute() {
     get("/client_dash") {
         // get client email from session
